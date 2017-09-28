55f1f74cb3e7c05f00e96cd408b0c3ac1d3b7960
imath <math.viet@gmail.com>
2017-09-29 00:51:02 +0200

    Add the email template

---
+++ email.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Email template used when sending emails.
+ *
+ * @package Vingt DixSept
+ *
+ * @since 1.0.0
+ */
+
+get_template_part( 'template-parts/email', 'header' );
+get_template_part( 'template-parts/email', 'body' );
